0603241bb108fe445acfe4a56375719f05143c4b
yankovskyi-ruslan <tital3108@gmail.com>
2023-02-28 17:59:22 +0200

    end hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import user from '../data/user.json';
+import stats from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+import { Profile } from './profile/profile';
+import { Statistics } from './Statistics/statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './Transaction/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile user={user} />
+      <Statistics title="Upload stats" stats={stats} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+    </>
   );
 };
